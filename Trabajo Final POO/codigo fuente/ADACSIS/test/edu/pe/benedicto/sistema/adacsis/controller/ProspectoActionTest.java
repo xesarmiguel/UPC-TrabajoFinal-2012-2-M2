22627d067f00adb874ae75d8ba993accb6494816
Cesar Miguel Illesca Cangalaya <xesarmiguel@gmail.com>
2012-11-19 13:07:45 -0500

    Creando el plan de prueba de Prospecto

---
+++ Trabajo Final POO/codigo fuente/ADACSIS/test/edu/pe/benedicto/sistema/adacsis/controller/ProspectoActionTest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.pe.benedicto.sistema.adacsis.controller;
+
+import edu.pe.benedicto.sistema.adacsis.view.ProspectoForm;
+import org.junit.Test;
+
+
+
+public class ProspectoActionTest {
+    
+    @Test
+    public void filtrar(ProspectoForm Form){
+        
+    }
+    
+}
