a3a91495ff360e29413336b8321acd1212dca3b6
Cesar Miguel Illesca Cangalaya <xesarmiguel@gmail.com>
2012-11-19 19:05:16 -0500

    Creando el plan de prueba de Prospecto

--- Trabajo Final POO/codigo fuente/ADACSIS/src/edu/pe/benedicto/sistema/adacsis/model/ProspectoDao.java
+++ Trabajo Final POO/codigo fuente/ADACSIS/src/edu/pe/benedicto/sistema/adacsis/model/ProspectoDao.java
@@ -1,13 +1,40 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package edu.pe.benedicto.sistema.adacsis.model;
 
-/**
- *
- * @author Admin
- */
+import edu.pe.benedicto.sistema.adacsis.bean.Persona;
+import edu.pe.benedicto.sistema.adacsis.bean.Prospecto;
+import java.util.ArrayList;
+import java.util.List;
+
 public class ProspectoDao {
+    public static List<Prospecto> objL_listaProspecto = new ArrayList<Prospecto>();
     
+    public boolean filtrar(Prospecto objA_entidad){        
+        List<Prospecto> objL_listaProspectoT = new ArrayList<Prospecto>();
+        for(Prospecto objA_prospecto : objL_listaProspecto){
+            Persona objL_persona = objA_prospecto.getObjPv_persona();
+            if (
+                objL_persona.getStrPv_nombre().equals(objL_persona.getStrPv_nombre()) ||
+                objL_persona.getStrPv_apellidoMaterno().equals(objL_persona.getStrPv_apellidoMaterno()) ||
+                objL_persona.getStrPv_apellidoPaterno().equals(objL_persona.getStrPv_apellidoPaterno()) ||
+                objL_persona.getStrPv_celular().equals(objL_persona.getStrPv_celular()) ||
+                objL_persona.getStrPv_telefono().equals(objL_persona.getStrPv_telefono())    
+            ){
+                objL_listaProspectoT.add(objA_prospecto);
+            }            
+        }
+        return objL_listaProspectoT.size() > 0; 
+    }
+    
+    public boolean insertar(Prospecto objA_entidad){        
+        return ProspectoDao.objL_listaProspecto.add(objA_entidad);
+    }
+    
+    public boolean eliminar(Prospecto objA_entidad){        
+        return ProspectoDao.objL_listaProspecto.remove(objA_entidad);
+    }
+    
+    public boolean actualizar(Prospecto objA_entidad){        
+        return ProspectoDao.objL_listaProspecto.add(objA_entidad);
+    }
+            
 }
