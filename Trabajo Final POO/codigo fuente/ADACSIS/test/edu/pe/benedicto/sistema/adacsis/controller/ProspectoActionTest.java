7812f108cd98eb24fb4612a99eca3015b7ecff55
Cesar Miguel Illesca Cangalaya <xesarmiguel@gmail.com>
2012-11-19 19:05:28 -0500

    Creando el plan de prueba de Prospecto

--- Trabajo Final POO/codigo fuente/ADACSIS/test/edu/pe/benedicto/sistema/adacsis/controller/ProspectoActionTest.java
+++ Trabajo Final POO/codigo fuente/ADACSIS/test/edu/pe/benedicto/sistema/adacsis/controller/ProspectoActionTest.java
@@ -4,16 +4,24 @@
  */
 package edu.pe.benedicto.sistema.adacsis.controller;
 
+import edu.pe.benedicto.sistema.adacsis.bean.Persona;
+import edu.pe.benedicto.sistema.adacsis.bean.Prospecto;
 import edu.pe.benedicto.sistema.adacsis.view.ProspectoForm;
 import org.junit.Test;
-
+import static org.junit.Assert.assertTrue;
 
 
 public class ProspectoActionTest {
     
     @Test
-    public void filtrar(ProspectoForm Form){
-        
+    public void filtrar(ProspectoForm objA_form){
+        ProspectoAction objL_prospectoAction = new ProspectoAction();                       
+        assertTrue(objL_prospectoAction.filtrar(objA_form));
+    }
+    
+    public void insertar(ProspectoForm objA_form){
+        ProspectoAction objL_prospectoAction = new ProspectoAction();                       
+        assertTrue(objL_prospectoAction.insertar(objA_form));
     }
     
 
